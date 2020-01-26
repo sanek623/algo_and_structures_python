1f8eecb99d551b1ce3d9a55ec7b77839dfa1a758
sanek <sanek@debian>
2020-01-26 22:11:32 +0400

    Lesson_5

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,31 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+from collections import namedtuple
+
+n = int(input("Введите кол-во пердприятий: "))
+firms = namedtuple('Firm', 'name quarter_1 quarter_2 quarter_3 quarter_4')
+profit = {}
+
+for i in range(n):
+    firm = firms(name=input("Введите название фирмы: "),
+                 quarter_1=int(input("Введите прибыль за 1 квартал: ")),
+                 quarter_2=int(input("Введите прибыль за 2 квартал: ")),
+                 quarter_3=int(input("Введите прибыль за 3 квартал: ")),
+                 quarter_4=int(input("Введите прибыль за 4 квартал: ")))
+    profit[firm.name] = (firm.quarter_1 + firm.quarter_2 + firm.quarter_3 + firm.quarter_4) / 4
+
+
+total = 0
+for value in profit.values():
+    total += value
+total = total / n
+
+for key, value in profit.items():
+    if value > total:
+        print(f'{key} - прибыль выше среднего!')
+    elif value < total:
+        print(f'{key} - прибыль ниже среднего!')
+    elif value == total:
+        print(f'{key} - средняя прибыль!')
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,18 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+import collections
+import functools
+
+nums = collections.defaultdict(list)
+for d in range(2):
+    n = input(f'Введите {d+1}-е натуральное шестнадцатеричное число: ')
+    nums[f'{d+1} {n}'] = list(n)
+print(nums)
+
+sum = sum([int(''.join(i), 16) for i in nums.values()])
+print('Сумма: ', list('%X' % sum))
+
+mult = functools.reduce(lambda a, b: a * b, [int(''.join(i), 16) for i in nums.values()])
+print('Произведение: ', list('%X' % mult))
